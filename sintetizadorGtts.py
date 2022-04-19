e35fcf90d91d482040ee4c7910c00487e046964a
logonrmlocal <natsu165@hotmail.com>
2022-04-19 11:46:43 -0300

    uso do os para executar o som

--- sintetizadorGtts.py
+++ sintetizadorGtts.py
@@ -1,9 +1,14 @@
 from gtts import gTTS
 from pygame import mixer
+#import os
 
-voz = gTTS("Olá mundo", lang='pt')
+voz = gTTS("Olá mundo", lang='pt', tld='com.br')
 voz.save('C:/Users/logonrmlocal/Desktop/audio.mp3')
 
 mixer.init()
 mixer.music.load("C:/Users/logonrmlocal/Desktop/audio.mp3")
 mixer.music.play()
+
+
+#Outra forma de executar
+#os.system('C:/Users/logonrmlocal/Desktop/audio.mp3')
