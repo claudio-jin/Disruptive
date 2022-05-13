860b672df6fee16858b8a7c47b88c6d5f0b62dbe
Claudio Jin <natsu165@hotmail.com>
2022-05-13 15:44:04 -0300

    Adicionado comando para pesquisar o clima de uma cidade

--- Checkpoint-DisruptiveIA/Checkpoint-Projeto-SextaFeira.py
+++ Checkpoint-DisruptiveIA/Checkpoint-Projeto-SextaFeira.py
@@ -2,6 +2,7 @@ import pyttsx3
 import speech_recognition as sr
 import os
 from datetime import datetime
+import requests
 
 recon = sr.Recognizer()
 comando = ""
@@ -25,13 +26,26 @@ def sexta_feira_escuta():
         return frase
 
 
+def buscar_clima(cidade):
+    API_KEY = "11c95aecdde84bed43cb10a7c167a494"
+    city = cidade
+
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=pt_br"
+    requisicao = requests.get(link)
+    dados = requisicao.json()
+    descricao = dados['weather'][0]['description']
+    temp = round(dados['main']['temp'] - 273.00, 1)
+
+    frase = "Hoje está {} na cidade {} e está fazendo {} graus".format(descricao, city, temp)
+
+    return frase
+
+
 sextaFeira = pyttsx3.init()
 sextaFeira.setProperty('voice', b'brasil')
 sextaFeira.setProperty('rate', 250)
 sextaFeira.setProperty('volume', 1)
 while True:
-    # sextaFeira.say("Diga alguma coisa: ")
-    # sextaFeira.runAndWait()
     resultado = sexta_feira_escuta()
 
     if resultado == "ok sexta feira" or resultado == "ok sexta-feira":
@@ -41,7 +55,8 @@ while True:
         while True:
             resp = sexta_feira_escuta()
             print("depois do while true: ", resp)
-            #cadastro de evento
+
+            # cadastro de evento
             if resp == "cadastrar evento na agenda":
                 with open("agenda.txt", 'a', encoding="utf-8") as f:
                     sextaFeira.say("Ok, qual evento devo cadastrar? ")
@@ -71,18 +86,25 @@ while True:
                         sextaFeira.runAndWait()
 
                     else:
-                        sextaFeira.say("O comando cadastrar encerrou")
+                        sextaFeira.say("O comando cadastrar evento encerrou")
                         sextaFeira.runAndWait()
 
-            #toque uma musica
+            #Ler agenda (if opcional)
+            if resp == "ler agenda" or resp == "leia agenda":
+                with open("./agenda.txt", 'r', encoding="utf-8") as agendaCadastrada:
+                    fala = ",".join(agendaCadastrada.readlines())
+                    print(fala)
+                    sextaFeira.say(fala)
+                    sextaFeira.runAndWait()
+
+            # toque uma musica
             if resp == "toque uma musica" or resp == "toque uma música":
                 sextaFeira.say("Ok mestre, abrindo mídia")
                 sextaFeira.runAndWait()
                 os.system(r"D:\akon-sryBlameItOnMe.mp3")
                 break
 
-
-            #Perguntando a data atual
+            # Perguntando a data atual
             if resp == "que dia é hoje?" or resp == "que dia é hoje":
                 meses = {1: "Janeiro", 2: "fevereiro", 3: "Março", 4: "Abril", 5: "Maio", 6: "Junho", 7: "Julho",
                          8: "Agosto", 9: "Setembro", 10: "Outubro", 11: "Novembro", 12: "Dezembro"}
@@ -99,7 +121,7 @@ while True:
                 sextaFeira.say(texto)
                 sextaFeira.runAndWait()
 
-            #perguntando que horas sao
+            # perguntando que horas sao
             if resp == "que horas são":
                 agora = datetime.now()
                 hora = agora.hour
@@ -110,17 +132,34 @@ while True:
                 sextaFeira.say(texto)
                 sextaFeira.runAndWait()
 
-            if resp == "abra a calculadora":
+            # abrir calculadora
+            if resp == "abra a calculadora" or resp == "abra calculadora":
                 sextaFeira.say("A calculadora está aberta, quais números deseja calcular?")
+                sextaFeira.runAndWait()
                 frase = sexta_feira_escuta()
                 calcular = frase.split()
                 if calcular[1] == 'x':
-                    print(int(calcular[0]) * int(calcular[2]))
+                    result = int(calcular[0]) * int(calcular[2])
+                    sextaFeira.runAndWait()
                 elif calcular[1] == '+':
-                    print(int(calcular[0]) + int(calcular[2]))
+                    result = int(calcular[0]) + int(calcular[2])
+                    sextaFeira.runAndWait()
                 elif calcular[1] == '-':
-                    print(int(calcular[0]) - int(calcular[2]))
+                    result = int(calcular[0]) - int(calcular[2])
+                    sextaFeira.runAndWait()
                 else:
-                    print(int(calcular[0]) / int(calcular[2]))
+                    result = int(calcular[0]) / int(calcular[2])
+                    sextaFeira.runAndWait()
+
+            # consultar o clima
+            if resp == "qual a previsão de hoje":
+                sextaFeira.say("Olá mestre, de qual cidade o senhor deseja saber o clima?")
+                sextaFeira.runAndWait()
+
+                resp = sexta_feira_escuta()
+
+                clima = buscar_clima(resp)
+                sextaFeira.say(clima)
+                sextaFeira.runAndWait()
     else:
-        print("nao entendi o que voce disse(else)")
+        print("Não entendi o que voce disse")
